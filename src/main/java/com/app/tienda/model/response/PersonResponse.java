8cc8a91bae01a126684e91b95569c15707a3396b
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-03-03 13:26:39 -0600

    feat(response): Add response alumno, person, policia

--- src/main/java/com/app/tienda/model/response/PersonResponse.java
+++ src/main/java/com/app/tienda/model/response/PersonResponse.java
@@ -1,4 +1,50 @@
 package com.app.tienda.model.response;
 
 public class PersonResponse {
+
+  private Long id;
+  private String name;
+  private String firstName;
+  private String secondName;
+  private String age;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getSecondName() {
+    return secondName;
+  }
+
+  public void setSecondName(String secondName) {
+    this.secondName = secondName;
+  }
+
+  public String getAge() {
+    return age;
+  }
+
+  public void setAge(String age) {
+    this.age = age;
+  }
 }
