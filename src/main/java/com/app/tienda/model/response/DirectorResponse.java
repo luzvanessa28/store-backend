653054c28c47cf31b73a5091717df0ed8ddf71c8
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-03-03 23:35:12 -0600

    feat(response): add director response

---
+++ src/main/java/com/app/tienda/model/response/DirectorResponse.java
@@ -0,0 +1,14 @@
+package com.app.tienda.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DirectorResponse {
+  private Long id;
+  private String name;
+  private String firstName;
+  private String secondName;
+  private String age;
+}
