2f003129e21ff879221d136f936a7b2f52d58ba7
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-03-04 21:22:09 -0600

    refactor(app): Add getAll method to retrieve all directors

--- src/main/java/com/app/tienda/controller/DirectorController.java
+++ src/main/java/com/app/tienda/controller/DirectorController.java
@@ -1,20 +1,46 @@
 package com.app.tienda.controller;
 
+import com.app.tienda.model.response.DirectorResponse;
+import com.app.tienda.service.IDirectorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/director")
 public class DirectorController {
   private final Logger log = LoggerFactory.getLogger(this.getClass());
 
+  @Autowired
+  private IDirectorService directorService;
+
   @GetMapping
-  public String getAll() {
+  public List<DirectorResponse> getAll() {
     log.info("Entrando a la funcion getAll");
-    return null;
+
+    return directorService.findAll();
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- src/main/java/com/app/tienda/service/impl/DirectorServiceImpl.java
+++ src/main/java/com/app/tienda/service/impl/DirectorServiceImpl.java
@@ -1,4 +1,36 @@
 package com.app.tienda.service.impl;
 
-public class DirectorServiceImpl {
+import com.app.tienda.entity.DirectorEntity;
+import com.app.tienda.model.response.DirectorResponse;
+import com.app.tienda.repository.DirectorRepository;
+import com.app.tienda.service.IDirectorService;
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class DirectorServiceImpl implements IDirectorService {
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+  @Autowired
+  private DirectorRepository directorRepository;
+  @Autowired
+  private ModelMapper modelMapper;
+
+  @Override
+  public List<DirectorResponse> findAll() {
+    log.info("FindAll en serviceImpl");
+
+    List<DirectorEntity> directors = directorRepository.findAll();
+    log.info("directors: {}", directors);
+
+    return directors.stream()
+      .map(directorEntity -> modelMapper.map(directorEntity, DirectorResponse.class))
+      .collect(Collectors.toList());
+  }
 }
