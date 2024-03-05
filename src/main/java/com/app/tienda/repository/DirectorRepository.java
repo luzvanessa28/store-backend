0be976a739beb914782c2af15aa24add517a16db
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-03-04 20:54:41 -0600

    refactor(repository): Add Director repository interface extending JpaRepository

--- src/main/java/com/app/tienda/repository/DirectorRepository.java
+++ src/main/java/com/app/tienda/repository/DirectorRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DirectorRepository extends JpaRepository<DirectorEntity> {
+public interface DirectorRepository extends JpaRepository<DirectorEntity, Long> {
 }
