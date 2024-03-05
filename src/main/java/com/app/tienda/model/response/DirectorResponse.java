de17aded027ff509dad3854d158a7ca1183b7e63
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-03-04 22:25:25 -0600

    refactor(response): Remove unused annotations in DirectorResponse

--- src/main/java/com/app/tienda/model/response/DirectorResponse.java
+++ src/main/java/com/app/tienda/model/response/DirectorResponse.java
@@ -1,12 +1,5 @@
 package com.app.tienda.model.response;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@ToString
 public class DirectorResponse {
   private Long id;
   private String name;
