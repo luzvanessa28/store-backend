ff022bc4a16876dccb3e8f0de4dd06dbf13e36e5
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-03-04 22:20:17 -0600

    refactor(response): add the @ToString to DirectorResponse

--- src/main/java/com/app/tienda/model/response/DirectorResponse.java
+++ src/main/java/com/app/tienda/model/response/DirectorResponse.java
@@ -2,9 +2,11 @@ package com.app.tienda.model.response;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class DirectorResponse {
   private Long id;
   private String name;
