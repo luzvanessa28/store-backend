b4af9b7d2fae00971bea193b092d104168ae5690
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-03-19 01:45:49 -0600

    feat(response): Add PerroResponse class

---
+++ src/main/java/com/app/tienda/model/response/PerroResponse.java
@@ -0,0 +1,31 @@
+package com.app.tienda.model.response;
+
+public class PerroResponse {
+  private String name;
+  private Integer age;
+  private String breed;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public String getBreed() {
+    return breed;
+  }
+
+  public void setBreed(String breed) {
+    this.breed = breed;
+  }
+}
