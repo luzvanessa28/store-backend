5ac374d575f42d9d963377b975052b5a8538624c
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-03-19 02:27:52 -0600

    feat(impl): add Implementation for PerroService

---
+++ src/main/java/com/app/tienda/service/impl/PerroServiceImpl.java
@@ -0,0 +1,9 @@
+package com.app.tienda.service.impl;
+
+import com.app.tienda.service.IPerroService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PerroServiceImpl implements IPerroService {
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
+}
