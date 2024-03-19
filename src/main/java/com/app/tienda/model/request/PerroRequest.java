2f97022d68f39bb562574fd1520587998016c26e
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-03-19 02:10:52 -0600

    feat(request): Add PerroRequest class to handle requests

---
+++ src/main/java/com/app/tienda/model/request/PerroRequest.java
@@ -0,0 +1,26 @@
+package com.app.tienda.model.request;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class PerroRequest {
+  @NotBlank(message = "El nombre no puede estar en blanco")
+  @Size(min = 3, max=20, message = "El nombre debe de contener de 3 a 20 caracteres")
+  private String name;
+  @NotBlank(message = "La edad no puede estar en blanco")
+  private Integer age;
+  @NotBlank(message = "La raza no puede estar en blanco")
+  private String breed;
+
+  public String getName() {
+    return name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public String getBreed() {
+    return breed;
+  }
+}
