dff950a84ebb6c220b9817b1a0119f30cdb2add3
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-03-18 18:26:50 -0600

    feat(controller): Add PerroController

--- src/main/java/com/app/tienda/controller/PerroController.java
+++ src/main/java/com/app/tienda/controller/PerroController.java
@@ -1,4 +1,12 @@
 package com.app.tienda.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/perros")
 public class PerroController {
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
 }
