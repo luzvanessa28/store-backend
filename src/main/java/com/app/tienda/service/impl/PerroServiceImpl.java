06e64c6d4c80d6e8ad8f0cf82055b735f3e0b6f2
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-03-19 20:23:30 -0600

    feat(service): Add the PerroServiceImpl service

--- src/main/java/com/app/tienda/service/impl/PerroServiceImpl.java
+++ src/main/java/com/app/tienda/service/impl/PerroServiceImpl.java
@@ -3,7 +3,9 @@ package com.app.tienda.service.impl;
 import com.app.tienda.service.IPerroService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PerroServiceImpl implements IPerroService {
   private final Logger log = LoggerFactory.getLogger(this.getClass());
 }
