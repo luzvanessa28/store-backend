8d881112099a566c50b47a5f40e5db13cf31c19d
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-03-29 23:13:26 -0600

    feat(app): added message constants

---
+++ src/main/java/com/app/tienda/constant/Message.java
@@ -0,0 +1,8 @@
+package com.app.tienda.constant;
+
+public class Message {
+  public static final String ID_NOT_FOUND = "El id no existe";
+  public static final String SAVE_ERROR = "Se produjo un error al guardar ";
+  public static final String UPDATE_ERROR = "Se produjo un error al actualizar ";
+
+}
