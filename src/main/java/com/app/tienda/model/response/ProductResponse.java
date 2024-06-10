e409e7844445cefe84930e8a45ba48ef03acb3b3
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-06-09 21:15:10 -0600

    refactor(productResponse): Added @Getter and @Setter annotations

--- src/main/java/com/app/tienda/model/response/ProductResponse.java
+++ src/main/java/com/app/tienda/model/response/ProductResponse.java
@@ -1,9 +1,14 @@
 package com.app.tienda.model.response;
 
 import com.app.tienda.entity.SupplierEntity;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.math.BigDecimal;
+
+@Getter
+@Setter
 @ToString
 public class ProductResponse {
   private Long id;
