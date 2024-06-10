6ef2c6f68f208a54ee66520a93599e9f40a3e6e4
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-06-09 21:26:18 -0600

    feat(productRepository): Added @Repository annotation to existing class

--- src/main/java/com/app/tienda/repository/ProductRepository.java
+++ src/main/java/com/app/tienda/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.app.tienda.repository;
 
 import com.app.tienda.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 }
