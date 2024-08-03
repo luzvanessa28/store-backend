78b31a8224fb4de36ce5ecfb525854d6ea5e71a1
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-08-02 20:58:26 -0600

    remove(alumno): removed unrelated files from the project

--- src/main/java/com/app/tienda/repository/AlumnoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.tienda.repository;
-
-import com.app.tienda.entity.AlumnoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlumnoRepository extends JpaRepository<AlumnoEntity, Long> {
-}
