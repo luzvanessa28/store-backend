2931711244f3d8f68836b6f848eef63d67a33578
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-09-04 22:54:15 -0600

    feat(message): define constants for error and success messages

--- src/main/java/com/app/tienda/constant/Message.java
+++ src/main/java/com/app/tienda/constant/Message.java
@@ -2,9 +2,11 @@ package com.app.tienda.constant;
 
 public class Message {
   // con la palabra final se define una constante
-  public static final String ID_NOT_FOUND = "El id no existe";
-  public static final String SAVE_ERROR = "Se produjo un error al guardar ";
-  public static final String UPDATE_ERROR = "Se produjo un error al actualizar ";
-  public static final String DELETE_ERROR = "Se produjo un error al eliminar ";
-
+  public static final String ID_NOT_FOUND = "The id does not exist";
+  public static final String SAVE_ERROR = "An error occurred while saving";
+  public static final String UPDATE_ERROR = "An error occurred while updating";
+  public static final String DELETE_ERROR = "An error occurred while deleting";
+  public static final String SAVE = "Successfully saved";
+  public static final String UPDATE = "Successfully updated";
+  public static final String DELETE = "Successfully deleted";
 }
