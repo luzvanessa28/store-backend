7a740cadd40941085e280ce584489a4e7b4807d0
luzvanessa28 <luzvanessa.ps@gmail.com>
2024-10-01 23:26:00 -0600

    "fix(Message): improve error messages for save, update, and delete operations"

--- src/main/java/com/app/tienda/constant/Message.java
+++ src/main/java/com/app/tienda/constant/Message.java
@@ -3,9 +3,9 @@ package com.app.tienda.constant;
 public class Message {
   // con la palabra final se define una constante
   public static final String ID_NOT_FOUND = "The id does not exist";
-  public static final String SAVE_ERROR = "An error occurred while saving";
-  public static final String UPDATE_ERROR = "An error occurred while updating";
-  public static final String DELETE_ERROR = "An error occurred while deleting";
+  public static final String SAVE_ERROR = "Error saving the record";
+  public static final String UPDATE_ERROR = "Error updating the record";
+  public static final String DELETE_ERROR = "Error deleting the record";
   public static final String SAVE = "Successfully saved";
   public static final String UPDATE = "Successfully updated";
   public static final String DELETE = "Successfully deleted";
